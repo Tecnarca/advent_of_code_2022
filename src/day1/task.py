2a7d97eed03440b529758859afb83a4f0cbe8b80
Tecnarca <giacomo.astolfi@prima.it>
2022-12-06 16:58:16 +0100

    Changed some days to only use 1 task file.

--- src/day1/task.py
+++ src/day1/task.py
@@ -4,3 +4,4 @@ with open("input") as csv_file:
     sums = [sum(map(int, row)) for row in reader(csv_file)]
 
 print(max(sums))
+print(sum(sorted(sums, reverse=True)[:3]))
--- src/day2/task.py
+++ src/day2/task.py
@@ -10,6 +10,20 @@ is_win = (
     ((strategy[0] == 3) & (strategy[1] == 1))
 )
 
-draw_score = is_draw.sum() * 3
-win_score = is_win.sum() * 6
-print(draw_score + win_score + strategy[1].sum())
+print(is_draw.sum() * 3 + is_win.sum() * 6 + strategy[1].sum())
+
+strategy[1] = strategy[1].replace({"X": 0, "Y": 3, "Z": 6})
+
+possible_round_scores = {
+    1: 3,
+    2: 1,
+    3: 2,
+    4: 1,
+    5: 2,
+    6: 3,
+    7: 2,
+    8: 3,
+    9: 1
+}
+
+print((strategy[1] + (strategy[0] + strategy[1]).replace(possible_round_scores)).sum())
