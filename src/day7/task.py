184fad5df71596ab77cd8cd284a1809c5a3532a0
Tecnarca <giacomo.astolfi@prima.it>
2022-12-07 09:12:38 +0100

    Readability, just a touch.

--- src/day7/task.py
+++ src/day7/task.py
@@ -2,9 +2,8 @@ from pathlib import Path
 from collections import defaultdict
 
 
-def parse_cd(path_string: str, pwd: Path) -> Path:
-    cd_path = Path(path_string)
-    return cd_path if cd_path.is_absolute() else Path((pwd / cd_path).resolve())
+def get_absolute_cd_path(path_string: Path, pwd: Path) -> Path:
+    return path_string if path_string.is_absolute() else Path((pwd / path_string).resolve())
 
 folders = defaultdict(lambda: 0)
 current_directory = Path("/")
@@ -13,7 +12,7 @@ with open("input") as input_file:
     for line_with_newline in input_file:
         line = line_with_newline[:-1]
         if line.startswith("$ cd"):
-            current_directory = parse_cd(line[5:], current_directory)
+            current_directory = get_absolute_cd_path(Path(line[5:]), current_directory)
         elif line[0].isdigit():
             size = int(line.split(" ")[0])
             folders[current_directory] += size
