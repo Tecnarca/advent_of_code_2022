43967799a3378e676fa0d9213f8b0126ac4bce4b
Tecnarca <giacomo.astolfi@prima.it>
2022-12-07 09:09:57 +0100

    Formatting

--- src/day7/task.py
+++ src/day7/task.py
@@ -1,5 +1,7 @@
 from pathlib import Path
 from collections import defaultdict
+
+
 def parse_cd(path_string: str, pwd: Path) -> Path:
     cd_path = Path(path_string)
     return cd_path if cd_path.is_absolute() else Path((pwd / cd_path).resolve())
@@ -17,8 +19,7 @@ with open("input") as input_file:
             for parent in current_directory.parents:
                 folders[Path(parent)] += size
 
-print(sum(size for size in folders.values() if size <= 100000))
-
 minimum_disk_space_to_free_up = 30000000 - (70000000 - folders[Path("/")])
 
+print(sum(size for size in folders.values() if size <= 100000))
 print(min(size for size in folders.values() if size >= minimum_disk_space_to_free_up))
